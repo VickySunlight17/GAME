0fe5f5f8644099f12dc79a2ba60c59b688c32c59
Виктория Потапенко <31548042+VickySunlight17@users.noreply.github.com>
2021-05-10 03:07:37 +0300

    Add files via upload

--- client/js/registration.js
+++ client/js/registration.js
@@ -21,10 +21,10 @@ document.querySelector('#create_game_btn').onclick = function() {
     document.querySelector('#game_options').style.display = "flex";
 }
 
-document.querySelector('#find_game_btn').onclick = function() {
-    document.querySelector('#choose_btn').style.display = "none";
-    document.querySelector('#list_of_games').style.display = "flex";
-}
+// document.querySelector('#find_game_btn').onclick = function() {
+//     document.querySelector('#choose_btn').style.display = "none";
+//     document.querySelector('#list_of_games').style.display = "flex";
+// }
 
 document.querySelector('#continue_game_btn').onclick = function() {
     document.querySelector('#choose_btn').style.display = "none";
@@ -41,10 +41,10 @@ document.querySelector('#create_game_btn2').onclick = function() {
     document.querySelector('#game_options').style.display = "flex";
 }
 
-document.querySelector('#start_game').onclick = function() {
-    document.querySelector('#enter_code').style.display = "none";
-    document.querySelector('.game_wrapper').style.display = "flex";
-}
+// document.querySelector('#start_game').onclick = function() {
+//     document.querySelector('#enter_code').style.display = "none";
+//     document.querySelector('.game_wrapper').style.display = "flex";
+// }
 
 document.querySelector('#start_game_btn').onclick = function() {
     document.querySelector('#game_options').style.display = "none";
